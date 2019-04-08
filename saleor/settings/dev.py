882bdc68d5b4c81fc79f7bc614c6cad3e2e6de86
Satyakam Pandya <satyakamp@nividata.com>
2019-04-08 15:33:51 +0530

    Color changes, dropdown fix, etc

--- saleor/settings/dev.py
+++ saleor/settings/dev.py
@@ -1,7 +1,9 @@
 from .base import *
+
 DATABASES = {
     'default': dj_database_url.config(
         default='postgres://kabdelmagid:Nicodem8s@localhost:5432/saleor',
+        # default='postgres://saleor_kelly:saleor_kelly@localhost:5432/saleor_kelly',
         conn_max_age=600)}
 
 SECRET_KEY = 'devel'
